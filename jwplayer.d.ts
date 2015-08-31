d0d2fa143d725561b2ff7a6c0a60d23d244c2de5
Andrew Prelusky <andrewgprelusky@gmail.com>
2015-08-31 15:44:45 -0400

    Added multiple descriptions for config options

--- jwplayer.d.ts
+++ jwplayer.d.ts
@@ -34,16 +34,34 @@ declare module JWPlayer {
      * A media item to play. These
      */
     interface MediaItem {
+           /**
+         * The title for your media item
+         */
         title?: string;
+           /**
+         * A description for the media item. This will appear under your title.
+         */
         description?: string;
+        /**
+         * A unique identifier used for JW Player analytics or personal tracking
+         */
         mediaid?: string;
 
         /**
-         * A poster image
+         * A poster image that appears when the player is idle
          */
         image?: string;
+        /**
+         * The file that will be played by JW Player
+         */
         file?: string;
+        /**
+         * A block used for configuring multiple media sources.
+         */
         sources?: Array<Source>;
+        /**
+         * Used for configuring VTT files for thumbnails and/or captions
+         */
         tracks?: Array<Track>;
     }
 
@@ -51,9 +69,21 @@ declare module JWPlayer {
      * A media file may have multiple backup sources to attempt playback from
      */
     interface Source {
+           /**
+         * The file that will load within the source
+         */
         file: string;
+           /**
+         * The label for this source
+         */
         label?: string;
+           /**
+         * Include and set to true to set as the default source
+         */
         default?: boolean;
+           /**
+         * Used to set the file type. Useful if you are including a dynamic file with no extension
+         */
         type?: string;
     }
 
@@ -69,7 +99,13 @@ declare module JWPlayer {
          * This has three acceptable values: captions, chapters or thumbnails
          */
         kind?: string;
+            /**
+         * The label that appears in the player interface during selection
+         */
         label?: string;
+            /**
+         * Include this and set it to true in order to make this a default track
+         */
         default?: boolean;
     }
 
@@ -78,43 +114,141 @@ declare module JWPlayer {
     }
 
     interface LogoConfig {
+            /**
+         * Your logo's image file
+         */
         file: string;
+            /**
+         * Setting to true will hide logo with control bar. false will display the logo at all times
+         */
         hide?: boolean;
+            /**
+         * Clicking the logo will direct users to this URL
+         */
         link?: string;
+            /**
+         * The logo's vertical and horizontal distance from the video border (In pixels)
+         */
         margin?: number;
+            /**
+         * Which corner the logo will appear in. (bottom-left, bottom-right, top-left, top-right)
+         */
         position?: string;
     }
 
     interface SetupConfig {
         playlist: Array<MediaItem>;
+        /**
+         * Used for responsive players. Must be in X:Y format.
+         */
         aspectratio?: any;
+        /**
+         * If true, will autostart player on page load
+         */
         autostart?: boolean;
+        /**
+         * Hides all controls. Requires an API method of starting.
+         */
         controls?: boolean;
+        /**
+         * The file configured to play on the player. Can also be configured in a playlist/source block
+         */
         file? : string;
+        /**
+         * The height (in pixels) at which the player will render.
+         */
         height?: number;
+        /**
+         * The poster image which will appear when the player is idle
+         */
         image?: string;
+        /**
+         * If true, the player will begin in a muted state
+         */
         mute?: boolean;
+        /**
+         * Set to Flash if you would like the player to render in Flash. This defaults to HTML5 otherwise.
+         */
         primary?: string;
+        /**
+         * If true, the player will repeat its playlist
+         */
         repeat?: boolean;
+        /**
+         * The width (in pixels) at which the player will render. If using a % for responsive design, be sure to use aspectratio.
+         */
         width?: number;
+        /**
+         * Defaults to true. Set to false to disable HLS on Android.
+         */
         androidhls?: boolean;
+        /**
+         * Used for overriding HLS qualities
+         */
         hlslabels?: any;
+        /**
+         * Defaults to true. Set false to hide the visual playlist.
+         */
         visualplaylist?: boolean;
+        /**
+         * Defaults to true. Set to false to hide your video's title.
+         */
         displaytitle?: boolean;
+        /**
+         * Defaults to true. Set to false to hide your video's description.
+         */
         displaydescription?: any;
+         /**
+         * The URL to an alternate location for our Flash provider (Defaults to our CDN)
+         */
         flashplayer?: string;
+         /**
+         * Changes video appearance based on player size. Can be uniform, fill, exactfit, none. (Defaults to uniform)
+         */
         stretching?: string;
+        /**
+         * Set to false to disable JW Player's quality/caption cookies
+         */
         cookies?: boolean;
+        /**
+         * Customizes player appearance via skins, active, inactive, and background configs. 
+         */
         skin?: any;
+          /**
+         * Used to configure player caption appearance
+         */
         captions?: {};
+          /**
+         * Used to configure a logo appearance and behavior
+         */
         logo?: LogoConfig;
+          /**
+         * Include this block to enable JW Player's sharing plugin
+         */
         sharing?: {};
+         /**
+         * Include this block to enable JW Player's GA plugin
+         */
         ga?: any;
+         /**
+         * Include this block to enable JW Player's related plugin
+         */
         related?: {};
-
+        /**
+         * Customizes player's right click text
+         */
         abouttext?: string;
+        /**
+         * Customizes player's right click link
+         */
         aboutlink?: string;
+        /**
+         * Used for configuring advertising features and plugins
+         */
         advertising?: any;
+        /**
+         * Used for configuring DRM features for DASH playback
+         */
         drm?: any;
     }
 
